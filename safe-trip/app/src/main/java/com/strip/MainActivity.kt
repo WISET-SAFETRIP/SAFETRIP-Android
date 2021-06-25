81dbf9e69e6ad01607df450311d41fa1cc37d604
Hee-jin506 <gmlwls35220@gmail.com>
2021-06-25 16:48:04 +0900

    #1 - listByInsideAndFieldNo 데이터 요청 후 결과 띄우기

--- safe-trip/app/src/main/java/com/strip/MainActivity.kt
+++ safe-trip/app/src/main/java/com/strip/MainActivity.kt
@@ -2,10 +2,50 @@ package com.strip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        thread(start=true) {
+            try {
+                var urlText = "http://10.0.2.2:8080/api/travels/listByInsideAndFieldNo/1"
+
+                val url = URL(urlText)
+
+                val urlConnection = url.openConnection() as HttpURLConnection
+                urlConnection.requestMethod = "GET"
+
+                val content = StringBuilder()
+
+                if (urlConnection.responseCode == HttpURLConnection.HTTP_OK) {
+                    val streamReader = InputStreamReader(urlConnection.inputStream)
+                    val buffered = BufferedReader(streamReader)
+
+                    while (true) {
+                        val line = buffered.readLine() ?: break
+                        content.append(line)
+                    }
+
+                    buffered.close()
+                }
+
+                urlConnection.disconnect()
+
+                runOnUiThread {
+                    Log.d("log", content.toString())
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        }
     }
 }
